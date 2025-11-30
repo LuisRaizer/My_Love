5902d15c11d3d0a376c83f9b8f8042740fcaad76
LuisRaizer <luispedrosrcruz@gmail.com>
2025-11-30 15:23:17 -0300

    [FIX] - Corrigido o gif

--- lib/components/home_component.dart
+++ lib/components/home_component.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:app/controllers/app_controller.dart';
 import 'package:app/controllers/timer_controller.dart';
 
-class HomeComponent extends StatefulWidget {
+class HomeComponent extends StatelessWidget {
   final AppController appController;
   final TimerController timerController;
 
@@ -13,28 +13,6 @@ class HomeComponent extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  _HomeComponentState createState() => _HomeComponentState();
-}
-
-class _HomeComponentState extends State<HomeComponent>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _bounceController;
-  late Animation<double> _bounceAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _bounceController = AnimationController(
-      duration: Duration(seconds: 3),
-      vsync: this,
-    )..repeat(reverse: true);
-
-    _bounceAnimation = Tween<double>(begin: 0, end: -15).animate(
-      CurvedAnimation(parent: _bounceController, curve: Curves.easeInOut),
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
@@ -42,57 +20,59 @@ class _HomeComponentState extends State<HomeComponent>
           backgroundColor: Colors.transparent,
           actions: [
             IconButton(
-              icon: Icon(
-                widget.appController.state.themeMode == ThemeMode.light
-                    ? Icons.nightlight_round
-                    : Icons.wb_sunny,
-              ),
-              onPressed: widget.appController.toggleTheme,
+              icon: Icon(appController.state.themeMode == ThemeMode.light
+                  ? Icons.nightlight_round
+                  : Icons.wb_sunny),
+              onPressed: appController.toggleTheme,
             ),
           ],
         ),
         SliverToBoxAdapter(
-          child: Column(children: [_buildSnoopyWithHouse(), _buildTitle()]),
+          child: Column(
+            children: [
+              _buildSnoopyHouse(),
+              _buildTitle(),
+              _buildWishButton(),
+            ],
+          ),
         ),
       ],
     );
   }
 
-  Widget _buildSnoopyWithHouse() {
+  Widget _buildSnoopyHouse() {
     return Container(
-      height: 400,
+      height: 350,
       child: Stack(
         children: [
           Center(
-            child: AnimatedBuilder(
-              animation: _bounceAnimation,
-              builder: (context, child) {
-                return Transform.translate(
-                  offset: Offset(0, _bounceAnimation.value),
-                  child: child,
-                );
-              },
-              child: GestureDetector(
-                onTap: () => widget.appController.incrementLove(),
-                child: Container(
-                  width: 280,
-                  height: 280,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.3),
-                        blurRadius: 10,
-                        offset: Offset(5, 5),
-                      ),
-                    ],
-                  ),
-                  child: Image.asset(
-                    'lib/assets/snoopy.gif',
-                    fit: BoxFit.contain,
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () => appController.incrementLove(),
+                  child: Container(
+                    width: 120,
+                    height: 120,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.black, width: 3),
+                    ),
+                    child: Image.asset(
+                      'lib/assets/snoopy.gif',
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
+                Container(
+                  width: 160,
+                  height: 140,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFe83f3f),
+                    border: Border.all(color: Colors.black, width: 3),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
@@ -101,39 +81,30 @@ class _HomeComponentState extends State<HomeComponent>
   }
 
   Widget _buildTitle() {
-    return Container(
-      margin: EdgeInsets.only(top: 10, bottom: 20),
-      child: Column(
-        children: [
-          Text(
-            'Parabéns Gadelha!',
-            style: TextStyle(
-              fontFamily: 'FredokaOne',
-              fontSize: 32,
-              color: Colors.white,
-              shadows: [
-                Shadow(
-                  offset: Offset(2, 2),
-                  blurRadius: 0,
-                  color: Colors.black,
-                ),
-                Shadow(
-                  offset: Offset(4, 4),
-                  blurRadius: 8,
-                  color: Colors.black.withOpacity(0.3),
-                ),
-              ],
-            ),
-            textAlign: TextAlign.center,
+    return Text(
+      'Parabéns!',
+      style: TextStyle(
+        fontFamily: 'FredokaOne',
+        fontSize: 32,
+        color: Colors.white,
+        shadows: [
+          Shadow(
+            offset: Offset(2, 2),
+            blurRadius: 0,
+            color: Colors.black,
           ),
         ],
       ),
     );
   }
 
-  @override
-  void dispose() {
-    _bounceController.dispose();
-    super.dispose();
+  Widget _buildWishButton() {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: ElevatedButton(
+        onPressed: appController.makeAWish,
+        child: Text('✨ Fazer um Pedido ✨'),
+      ),
+    );
   }
 }
